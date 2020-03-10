f24959c6fdd4f7e69f4e055d22a2edc66a464602
jihoon <jihoon@hancom.com>
2020-03-10 19:19:21 +0900

    Introduce a Chartist.JS example using Vue

--- vue-chart-test/src/App.vue
+++ vue-chart-test/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ChartistExample title="my chartist example" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChartistExample from './components/ChartistExample.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ChartistExample,
   }
 }
 </script>
---
+++ vue-chart-test/src/components/ChartistExample.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id='chart-user-count'>
+    <!-- TODO: handle the css properly -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+import Chartist from 'chartist';
+
+export default {
+  name: 'chartist-example',
+  props: {
+    title: String
+  },
+  mounted() {
+    console.log(this.title + " mounted.");
+
+    new Chartist.Line('#chart-user-count',
+      // data
+      {
+        labels: [
+          '2019 September',
+          '2019 October',
+          '2019 November',
+          '2019 December',
+          '2020 January',
+          '2020 February',
+          '2020 March'
+        ],
+        series: [[ 23452, 25262, 30760, 28960, 31234, 34503, 27689 ]]
+      },
+      // options
+      {
+        width: 800,
+        height: 600,
+        lineSmooth: Chartist.Interpolation.none({
+          fillHoles: false
+        }),
+      });
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
